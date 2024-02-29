a1e1b514f3c6772a1da60d3bdc80006f966c9dc8
grahamcassoutt <grahamc@jeveg.com>
2024-02-29 13:43:29 -0600

    initial

--- website/views.py
+++ website/views.py
@@ -5,4 +5,4 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     is_home_page = True
-    return render_template('home.html', is_home_page=is_home_page)
+    return '<h1>Hello, World!</h1>'
